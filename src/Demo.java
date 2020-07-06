8203aba5522fa30d5100130b64231ea0fd04a2e5
OmarInCS <omarincs@gmail.com>
2020-07-06 17:40:47 +0300

    Introduction

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Welcome to Java");
+        // System.out.println(5 / 0);   Runtime Error
+//        System.out.println(23 + 20 + 17 / 3);     Logic Error
+        System.out.println("5 * 5 => " + (5 * 5));
+    }
+
+
+}
