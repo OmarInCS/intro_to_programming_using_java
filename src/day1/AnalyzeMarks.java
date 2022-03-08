db15a4500ba65c4145949009f2de4c33db22307f
OmarInCS <omarincs@gmail.com>
2022-03-08 16:07:31 +0300

    day 1

---
+++ src/day1/AnalyzeMarks.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AnalyzeMarks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter three integers: ");
+        int m1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int m3 = sc.nextInt();
+
+        double avg = (m1 + m2 + m3) / 3.0;
+        System.out.println("Average: " + Math.round(avg));
+        System.out.println("Best: " + Math.max(Math.max(m1, m2), m3));
+        System.out.println("Worst: " + Math.min(Math.min(m1, m2), m3));
+    }
+}
---
+++ src/day1/Demo.java
@@ -0,0 +1,10 @@
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Java is Fun");
+        // System.out.println(5 / 0);   -- Runtime Error
+//        System.out.println(17 + 20 + 23 / 3); -- Logic Error
+        System.out.println("5 * 5 = " + 5 * 5);
+    }
+}
